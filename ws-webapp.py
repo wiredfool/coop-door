46adaa9e7da7b00c1ed58b179f7dbb7dfa84b950
wiredfool <eric-github@soroos.net>
2014-03-11 05:29:02 +0000

    flask/websockets version of the webapp

---
+++ ws-webapp.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+
+from flask import Flask, redirect, render_template
+from flask_sockets import Sockets
+
+from door import client
+client.connect()
+status_socket = client.status_socket()
+
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+app.debug = True
+
+@sockets.route('/status')
+def ws_status(ws):
+    while True:
+        message = status_socket.recv(1024)
+        ws.send(message)
+        
+@app.route('/')
+def index():
+    return render_template('base.html', page=client.status())
+                            
+@app.route('/close', methods=['POST'])
+def close_door():
+    client.close()
+    return redirect('/')
+
+@app.route('/open', methods=['POST'])
+def open_door():
+    client.open()
+    return redirect('/')
+
+@app.route('/stop', methods=['POST'])
+def stop():
+    client.stop()
+    return redirect('/')
+
+@app.route('/reload', methods=['POST'])
+def refresh():
+    client.reload()
+    return redirect('/')
+
+
