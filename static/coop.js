f8aedd8bc44c279ba496222cbad8134caf6a91da
wiredfool <eric-github@soroos.net>
2014-05-08 20:17:26 +0000

    basic svg animation of the open/close

--- static/coop.js
+++ static/coop.js
@@ -11,6 +11,7 @@
 
 var sock = new WebSocket('ws://'+ document.location.host + '/status');
 var elt = $('#status');
+var current_status = {}
 
 if (sock) {
     sock.onopen = function () {
@@ -18,7 +19,10 @@ if (sock) {
 		sock.send('ping');
     };
     sock.onmessage = function (event) {
-        elt.html(event.data);
+		var status = parse_response(event.data)
+        elt.html(JSON.stringify(status))
+		update_svg(status)
+		current_status = status;
     };
     sock.onclose = function () {
         elt.html('no data');
@@ -30,3 +34,30 @@ $('form.inline').submit(function(e){
     $.post(this.action);
 	return false;
 })
+
+
+function parse_response(s) {
+	var elts = s.split('\n')
+	console.log(elts)
+	var status ={}
+	if (elts.length){
+		try {
+			status = JSON.parse(elts[elts.length-1])
+		} catch(e) {
+			console.log(e)
+		}
+	}
+	return status
+}
+
+function update_svg(status) {
+	$('#door').removeClass()
+	$('#door').addClass(status.state)
+	if (status.upper){
+		$('#door').addClass('upper')
+	}
+	if (status.lower){
+		$('#door').addClass('lower')
+	}
+
+}
