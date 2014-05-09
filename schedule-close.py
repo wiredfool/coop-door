993a2d98b4b6c991718b58f2f56eec1710282e54
wiredfool <eric-github@soroos.net>
2014-05-08 22:38:42 -0700

    use the current executable, enabling virtualenv

--- schedule-close.py
+++ schedule-close.py
@@ -3,7 +3,7 @@
 # Schedule the door to close at the end of civil twilight, using the astral library. 
 
 
-import os
+import os, sys
 from astral import Astral
 
 city_name = 'Seattle'
@@ -15,5 +15,7 @@ city = astral[city_name]
 
 sun = city.sun(local=True)
 
-os.system("""echo '/usr/bin/python %s' | at %s""" % 
-          (os.path.abspath('door/close.py'), sun['dusk'].strftime("%H:%M")))
+os.system("""echo '%s %s' | at %s""" % 
+          (sys.executable, 
+           os.path.abspath('door/close.py'), 
+           sun['dusk'].strftime("%H:%M")))
